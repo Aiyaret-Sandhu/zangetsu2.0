db4ad8809af10c4903a0abbc28b2d91d1ddb8d94
Aiyaret-Sandhu <arshsandhuprofessional@gmail.com>
2024-09-24 20:30:36 +0530

    24092024-vercel.json

---
+++ src/dbconfig/firebase.js
@@ -0,0 +1,11 @@
+const firebase = {
+    apiKey: "AIzaSyCrIeZBwZCfdpGlk1T7pvt9GONQpp7BXFc",
+    authDomain: "zangetsu-f1369.firebaseapp.com",
+    projectId: "zangetsu-f1369",
+    storageBucket: "zangetsu-f1369.appspot.com",
+    messagingSenderId: "429884960669",
+    appId: "1:429884960669:web:339559415ed69e77290974"
+  };
+
+
+export default firebase;
